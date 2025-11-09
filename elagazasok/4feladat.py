35e1845e1fb0862f1ef5c9e3a7273284eecc98a9
Bujdoso Balint <bujdosobalint8@gmail.com>
2025-11-09 10:44:06 +0100

    u

--- elagazasok/4feladat.py
+++ elagazasok/4feladat.py
@@ -4,9 +4,9 @@ def divisors():
     num2=int(str[1])
     if(num1 == num2):
         print(f"A megadott adatok egyenlőek")
-    elif(num1 % num2):
+    elif(num1 % num2==0):
         print(f"{num1} {num2} osztója")
-    elif(num2 % num1):
+    elif(num2 % num1==0):
         print(f"{num2} {num1} osztója")
     else:
         print("Relatív prímek")
