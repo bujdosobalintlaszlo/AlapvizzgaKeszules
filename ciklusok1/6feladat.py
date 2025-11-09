3b34c8d073c3d573ad928e9749baa46b48f43eeb
Bujdoso Balint <bujdosobalint8@gmail.com>
2025-11-09 10:28:49 +0100

    up

--- ciklusok1/6feladat.py
+++ ciklusok1/6feladat.py
@@ -1,6 +1,13 @@
-def gcd(n1,n2):
-    i=1
-    smaller= n1 <n2 ? n1 : n2
-    while(i*i<smaller):
-        
-        i+=1
+def gcd(n1, n2):
+    smaller = n1 if n1 < n2 else n2
+    gcd_value = 1
+    for i in range(1, smaller + 1):
+        if n1 % i == 0 and n2 % i == 0:
+            gcd_value = i
+    return gcd_value
+
+#eukledeszi
+def gcdE(n1, n2):
+    while n2 != 0:
+        n1, n2 = n2, n1 % n2
+    return n1
